0b0a1c5373ade478f8219461509ecf9c8001607d
Tasin Zaman <61430246+ZmnTasin@users.noreply.github.com>
2020-05-18 01:10:35 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var randomNumber1 = Math.floor((Math.random()*6)+1);
+document.querySelector(".img1").setAttribute("src" , ("dice"+randomNumber1+".png"));
+
+var randomNumber2 = Math.floor((Math.random()*6)+1);
+document.querySelector(".img2").setAttribute("src" , ("dice"+randomNumber2+".png"));
+
+if (randomNumber1>randomNumber2) {
+  document.querySelector("h1").innerText = "Player 1 Wins";
+} else if (randomNumber1<randomNumber2) {
+  document.querySelector("h1").innerText = "Player 2 Wins";
+} else {
+  document.querySelector("h1").innerText = "It's a Tie";
+}
